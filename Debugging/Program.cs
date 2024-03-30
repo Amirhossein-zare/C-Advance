cf83f94db4beefc90597b15b9028a76791432823
AmirHossein Zare <130905177+Amirhossein-zare@users.noreply.github.com>
2024-03-30 12:08:03 +0330

    Delegates And Thracing
    how to thrace and break

---
+++ Debugging/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Debugging
+{
+    internal class Program
+    {
+        public delegate void PrintClass(int number);
+        static void Main(string[] args)
+        {
+            //Thrace
+            //break point
+            PrintClass amir = PrintA;
+            Print(amir,26);
+        }
+        public static void Print(PrintClass a,int b)
+        {
+            a(b);
+        }
+        public static void PrintA(int number)
+        {
+            Console.WriteLine("Ali"+number);
+        }
+    }
+}
