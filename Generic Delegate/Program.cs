1f5d4f2557e7e6dfebe7e539fe407026a558be39
AmirHossein Zare <130905177+Amirhossein-zare@users.noreply.github.com>
2024-03-31 00:03:11 +0330

    Generic Delegate
    how to create generic delegates

---
+++ Generic Delegate/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Delegate
+{
+    internal class Program
+    {
+        public delegate void MyDelegate1<T1, T2>(T1 a , T2 b);
+
+        public delegate T3 MyDelegate2<T1, T2, T3>(T1 a, T2 b);
+        static void Main(string[] args)
+        {
+            MyDelegate2<string, bool, int> del1 = Fun4;
+            Console.WriteLine(del1.Invoke("amir",false));
+
+
+
+            MyDelegate1<int, string> a1 = Fun1;
+            a1.Invoke(22, "amir");
+        }
+
+        public static void Fun1(int x , string y)
+        {
+            Console.WriteLine("X is : {0}\tY is : {1}",x,y);
+        }
+        public static void Fun2(float x , bool  y)
+        {
+            Console.WriteLine("X is : {0}\tY is : {1}",x,y);
+        }
+        public static void Fun3(float x , double y)
+        {
+            Console.WriteLine("X is : {0}\tY is : {1}",x,y);
+        }
+        public static int Fun4(string name, bool flag)
+        {
+            Console.WriteLine("Hello {0}",name);
+            return flag ?  0 : 1 ;
+        }
+    }
+}
