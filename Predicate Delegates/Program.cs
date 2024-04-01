111204866596bba2f33f68fe5909c675670fe890
AmirHossein Zare <130905177+Amirhossein-zare@users.noreply.github.com>
2024-04-01 21:36:29 +0330

    Predicate Delegates
    how to create predicate delegates

---
+++ Predicate Delegates/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Predicate_Delegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<int> age = AgeValidate;
+            Console.WriteLine(age.Invoke(22));
+        }
+        public static bool AgeValidate(int age)
+        {
+            return age > 0 && age < 100;//? true : false;
+        }
+    }
+}
