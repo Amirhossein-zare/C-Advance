d31373396ccad6a8ec82c1948b8c5c58a512a5cb
AmirHossein Zare <130905177+Amirhossein-zare@users.noreply.github.com>
2024-04-04 14:41:09 +0330

    Object Collections With Method Syntax
    how to create collection and methods with method syntax

---
+++ Object Collections With Method Syntax/Person.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_Collections_With_Method_Syntax
+{
+    internal class Person
+    {
+        public int PersonId { get; set; }
+        public string Name { get; set; }
+        public string Family { get; set; }
+
+        public Person(int PersonId, string Name, string Family)
+        {
+            this.PersonId = PersonId;
+            this.Name = Name;
+            this.Family = Family;
+        }
+        public override string ToString()
+        {
+            return string.Format("Id : {0}\tName : {1}\tFamily : {2}",PersonId,Name,Family);
+        }
+    }
+}
---
+++ Object Collections With Method Syntax/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_Collections_With_Method_Syntax
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Person person1 = new Person(1,"amir","zare");
+
+            //Console.WriteLine(person1.ToString());
+
+            List<Person> personList = new List<Person>()
+            {
+                new Person(1,"amir","zare"),
+                new Person(2,"arshia","zare"),
+                new Person(3,"hanie","esmail"),
+                new Person(4,"faeze","esmail"),
+                new Person(5,"ali","taheri"),
+                new Person(6,"faeze","esmail")
+            };
+
+            //Method Syntax
+            var list1 = personList.Where(  t => t.Name.StartsWith("h") && t.Family.EndsWith("l")  );
+            var list1 = personList.OrderBy( t => t.Name ).ThenBy( t => t.Family );
+            var list1 = personList.OrderBy(t => t.Name).ThenBy(t => t.Family).ThenBy(t => t.PersonId);
+
+            //Query Syntax
+            var list1 = from t in personList
+            where t.Name.StartsWith("")
+            orderby t.Name ascending
+            select t;
+
+            foreach (var person in list1)
+            {
+                Console.WriteLine(person.ToString());
+            }
+
+        }
+    }
+}
